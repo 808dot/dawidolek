c25cd3ac0b3a432e066729b9a4d31ca475b76f07
Dawid <dawidolek808@gmail.com>
2023-11-06 16:16:23 +0100

    Init commit

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var toAdd = document.getElementsByClassName(".icons-header");
+console.log(toAdd);
+// var landingPage = document.createElement('div');
+// landingPage.className = 'landing';
+// toAdd.appendChild(landingPage);
