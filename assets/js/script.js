b9a5fabe669c50e49fe87e0f3cbe34c761329ef4
Dawid <dawidolek808@gmail.com>
2023-11-08 01:35:46 +0100

    Some buttons functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,28 @@
-var toAdd = document.getElementsByClassName(".icons-header");
-console.log(toAdd);
-// var landingPage = document.createElement('div');
-// landingPage.className = 'landing';
-// toAdd.appendChild(landingPage);
+var Arrows = document.querySelectorAll(".info-case i");
+var InfoCase = document.querySelectorAll(".info-case");
+console.log(Arrows);
+
+
+
+
+Arrows.forEach((Arrow, index) => {
+    Arrow.addEventListener('click', () => {
+        // description.style.visibility = 'visible';
+        console.log(index)
+        if (InfoCase[index].style.height != '100px' ){
+            InfoCase[index].style.setProperty('height', '100px');
+        } else { 
+            InfoCase[index].style.setProperty('height', '50px');
+        }
+            
+        
+    });
+});
+  
+
+// descriptions.forEach( (description) => {
+//     description.addEventListener("mouseover", () => {
+//       // do something when the button is clicked
+//       console.log("You clicked a button");
+//     });
+//   });
