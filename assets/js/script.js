6581485fcfa2258685dd7b174b61e0483eb8a8a6
Dawid <dawidolek808@gmail.com>
2023-11-11 22:42:30 +0100

    New icons, Hover section action

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,20 @@
-var Arrows = document.querySelectorAll(".info-case i");
-var InfoCase = document.querySelectorAll(".info-case");
-console.log(Arrows);
+let Arrows = document.querySelectorAll(".info-case i");
+let InfoCases = document.querySelectorAll(".info-case");
+let ProjectsIcons = document.querySelectorAll(".info-case .left img");
+let ProjectsTitle = document.querySelectorAll(".info-case .left p");
+// console.log(Arrows);
 
 
 
-
+// Clicked arrow in ifno case make window bigger
 Arrows.forEach((Arrow, index) => {
     Arrow.addEventListener('click', () => {
         // description.style.visibility = 'visible';
         console.log(index)
-        if (InfoCase[index].style.height != '100px' ){
-            InfoCase[index].style.setProperty('height', '100px');
+        if (InfoCases[index].style.height != '100px' ){
+            InfoCases[index].style.setProperty('height', '100px');
         } else { 
-            InfoCase[index].style.setProperty('height', '50px');
+            InfoCases[index].style.setProperty('height', '50px');
         }
             
         
@@ -20,9 +22,43 @@ Arrows.forEach((Arrow, index) => {
 });
   
 
-// descriptions.forEach( (description) => {
-//     description.addEventListener("mouseover", () => {
-//       // do something when the button is clicked
-//       console.log("You clicked a button");
-//     });
-//   });
+// Hover info-case
+
+InfoCases.forEach((InfoCase, index) => {
+    // console.log(InfoCase)
+    InfoCase.addEventListener("mouseover", (event) => {
+          // highlight the mouseover target
+          console.log(InfoCase);
+          InfoCase.style.setProperty('align-items', 'start');
+          ProjectsIcons[index].style.setProperty('width', '28px');
+          ProjectsTitle[index].style.setProperty('font-size', '28px');
+        //   InfoCase.style.setProperty('background', 'green');
+      
+          // reset the color after a short delay
+        //   setTimeout(() => {
+        //     event.target.style.color = "";
+        //   }, 500);
+        },
+        false,
+      );
+});
+
+InfoCases.forEach((InfoCase, index) => {
+    // console.log(InfoCase)
+    InfoCase.addEventListener("mouseout", (event) => {
+          // highlight the mouseover target
+          console.log(index);
+        //   InfoCase.style.removeProperty('align-items');
+          ProjectsIcons[index].style.removeProperty('width');
+          ProjectsTitle[index].style.removeProperty('font-size');
+        //   InfoCase.style.setProperty('background', 'green');
+      
+          // reset the color after a short delay
+        //   setTimeout(() => {
+        //     event.target.style.color = "";
+        //   }, 500);
+        },
+        false,
+      );
+});
+
